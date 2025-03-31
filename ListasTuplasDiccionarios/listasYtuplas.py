b7242015a6c8b6c1c81e0a0553b651cfc12e8de7
danielBeta25036 <danielbeta402@gmail.com>
2025-03-31 07:45:52 -0500

    Add files via upload

---
+++ ListasTuplasDiccionarios/listasYtuplas.py
@@ -0,0 +1,14 @@
+#PARA LSITAS
+
+my_lista = ['Rojo', 'Azul', 'Amarillo', 'Naranja', 'Violeta', 'Verde']
+print(my_lista)
+print(type(my_lista))
+print(my_lista[2])
+
+print("my_lista size: ", len(my_lista))
+print(my_lista[0:2])
+print(my_lista[:2])
+
+my_lista.append('blanco')
+print(my_lista)
+
