dcdd246e9749c1371cbbde5915cc0a4936179f13
danielBeta25036 <danielbeta402@gmail.com>
2025-05-12 07:19:12 -0500

    Update Strings.py

--- Corte2/Strings.py
+++ Corte2/Strings.py
@@ -5,3 +5,44 @@ print(s.index("a"))
 
 print(s[0])
 print(s[3:5])
+
+
+# this will give us an error
+s[0] = "b"
+
+print('h' in 'abcd') # False
+
+print('ab' in 'abcd') # False
+
+
+print(['a', 'b'] in ['a', 'b', 'c', 'd']) # False
+
+
+abc_list = list("abracadabra")
+print(abc_list)
+
+l = ['a', 'b', 'r', 'a', 'c', 'a', 'd', 'a', 'b', 'r', 'a']
+
+s = "".join(l)
+print(s)
+animals = ('cat', 'dog', 'fish')
+
+# a space-separated list
+print(" ".join(animals))
+
+# a comma-separated list
+print(",".join(animals))
+
+# a comma-separated list with spaces
+print(", ".join(animals))
+
+print("cat    dog fish\n".split())
+
+
+print("cat|dog|fish".split("|"))
+
+
+print("cat, dog, fish".split(", "))
+
+print("cat, dog, fish".split(", ", 2))
+
